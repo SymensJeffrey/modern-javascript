43584033f0148969f686cb92560d848113edc90e
Jeffrey Symens <jeffrey.symens23@gmail.com>
2021-07-14 19:40:28 -0500

    add webpack

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-/* global Vue, axios */
-var app = new Vue({
-  el: "#app",
-  data: function () {
-    return {
-      message: "Hello from JavaScript!",
-    };
-  },
-});
 
-
-
+console.log(moment().calendar());
